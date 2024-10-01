6b2738955b6727a121f15f347807a424a1028adc
saseum <dvlpjh@gmail.com>
2024-10-01 18:29:27 +0900

    view: basic templates using Thymeleaf and js file

---
+++ src/main/resources/static/js/article.js
@@ -0,0 +1,66 @@
+
+// 삭제 기능
+const deleteButton = document.getElementById('delete-btn');
+
+if (deleteButton) {
+    deleteButton.addEventListener('click', event => {
+        if (confirm('글을 삭제하시겠습니까?')) {
+            let id = document.getElementById('article-id').value;
+            fetch(`/api/articles/${id}`, {
+                method: 'DELETE'
+            })
+            .then(() => {
+                alert('삭제 완료');
+                location.replace('/articles');
+            });
+        }
+})
+
+}
+
+// 수정 기능
+const modifyButton = document.getElementById('modify-btn');
+
+if (modifyButton) {
+    modifyButton.addEventListener('click', event => {
+        let params = new URLSearchParams(location.search);
+        let id = params.get('id');
+
+        fetch(`/api/articles/${id}`, {
+          method: 'PUT',
+          headers: {
+              "Content-Type": 'application/json',
+          },
+          body: JSON.stringify({
+              title: document.getElementById('title').value,
+              content: document.getElementById('content').value
+          })
+        })
+        .then(() => {
+            alert('수정 완료');
+            location.replace(`/articles/${id}`);
+        });
+    });
+}
+
+// 등록 기능
+const createButton = document.getElementById('create-btn');
+
+if (createButton) {
+    createButton.addEventListener('click', event => {
+       fetch("/api/articles", {
+           method: "POST",
+           headers: {
+               "Content-Type": "application/json",
+           },
+           body: JSON.stringify({
+               title: document.getElementById('title').value,
+               content: document.getElementById('content').value,
+           }),
+       })
+       .then(() => {
+           alert('등록 완료');
+           location.replace("/articles");
+       });
+    });
+}
