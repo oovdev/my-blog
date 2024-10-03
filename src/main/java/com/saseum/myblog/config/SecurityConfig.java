1c65efeb5618634d496179a9903356fb7df8b859
saseum <dvlpjh@gmail.com>
2024-10-03 21:07:42 +0900

    modified: Spring Security OAuth2 관련 변경

--- src/main/java/com/saseum/myblog/config/SecurityConfig.java
+++ src/main/java/com/saseum/myblog/config/SecurityConfig.java
@@ -1,32 +1,73 @@
 package com.saseum.myblog.config;
 
+import com.saseum.myblog.config.jwt.TokenProvider;
+import com.saseum.myblog.config.oauth.OAuth2AuthorizationRequestBasedOnCookieRepository;
+import com.saseum.myblog.config.oauth.OAuth2FailureHandler;
+import com.saseum.myblog.config.oauth.OAuth2SuccessHandler;
+import com.saseum.myblog.config.oauth.OAuth2UserCustomService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+@RequiredArgsConstructor
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
+    private final OAuth2UserCustomService oAuth2UserCustomService;
+    private final TokenProvider tokenProvider;
+    private final OAuth2AuthorizationRequestBasedOnCookieRepository oAuth2AuthorizationRequestBasedOnCookieRepository;
+    private final OAuth2SuccessHandler oAuth2SuccessHandler;
+    private final OAuth2FailureHandler oAuth2FailureHandler;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/login", "/sign-up", "/user", "/static/**", "/h2-console/**").permitAll()
-                        .anyRequest().authenticated())
+                .csrf(csrf -> csrf.disable())
+                .formLogin(form -> form.disable())
                 .headers(header -> header
                         .frameOptions(option -> option.disable()))
-                .formLogin(form -> form
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/login", "/sign-up", "/user", "/js/**", "/img/**", "/css/**", "/h2-console/**").permitAll()
+                        .requestMatchers("/api/token").permitAll()
+                        .requestMatchers("/api/**").authenticated()
+                        .anyRequest().permitAll()
+                )
+                .oauth2Login(oauth -> oauth
                         .loginPage("/login")
-                        .defaultSuccessUrl("/articles"))
+                        .successHandler(oAuth2SuccessHandler)
+                        .failureHandler(oAuth2FailureHandler)
+                        .authorizationEndpoint(endpoint -> endpoint
+                                .authorizationRequestRepository(oAuth2AuthorizationRequestBasedOnCookieRepository)
+                        )
+                        .userInfoEndpoint(endpoint -> endpoint
+                                .userService(oAuth2UserCustomService))
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
                 .logout(logout -> logout
                         .logoutSuccessUrl("/login")
-                        .invalidateHttpSession(true))
-                .csrf(csrf -> csrf
-                        .disable())
+                )
+                .exceptionHandling(config -> config
+                        .defaultAuthenticationEntryPointFor(
+                                new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED),
+                                new AntPathRequestMatcher("/api/**")
+                        )
+                )
                 .build();
+    }
+
+    @Bean
+    public TokenAuthenticationFilter tokenAuthenticationFilter() {
+        return new TokenAuthenticationFilter(tokenProvider);
     }
 
     @Bean
