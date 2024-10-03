0a0871b61da35f4cde2e66ecc20eadaa55b909bc
saseum <dvlpjh@gmail.com>
2024-10-03 21:05:21 +0900

    modified: OAuth2 Login 관련 뷰 및 static 자원  구성

--- src/main/resources/static/js/article.js
+++ src/main/resources/static/js/article.js
@@ -3,43 +3,43 @@
 const deleteButton = document.getElementById('delete-btn');
 
 if (deleteButton) {
-    deleteButton.addEventListener('click', event => {
-        if (confirm('글을 삭제하시겠습니까?')) {
-            let id = document.getElementById('article-id').value;
-            fetch(`/api/articles/${id}`, {
-                method: 'DELETE'
-            })
-            .then(() => {
-                alert('삭제 완료');
-                location.replace('/articles');
-            });
-        }
-})
+    deleteButton.addEventListener("click", (event) => {
+        let id = document.getElementById("article-id").value;
 
+        function success() {
+            alert("삭제가 완료.");
+            location.replace("/articles");
+        }
+
+        function fail() {
+            alert("삭제 실패.");
+            location.replace("/articles");
+        }
+
+        httpRequest("DELETE", "/api/articles/" + id, null, success, fail);
+    });
 }
 
 // 수정 기능
 const modifyButton = document.getElementById('modify-btn');
 
 if (modifyButton) {
-    modifyButton.addEventListener('click', event => {
+    modifyButton.addEventListener("click", (event) => {
         let params = new URLSearchParams(location.search);
-        let id = params.get('id');
-
-        fetch(`/api/articles/${id}`, {
-          method: 'PUT',
-          headers: {
-              "Content-Type": 'application/json',
-          },
-          body: JSON.stringify({
-              title: document.getElementById('title').value,
-              content: document.getElementById('content').value
-          })
-        })
-        .then(() => {
-            alert('수정 완료');
-            location.replace(`/articles/${id}`);
+        let id = params.get("id");
+        body = JSON.stringify({
+            title: document.getElementById("title").value,
+            content: document.getElementById("content").value,
         });
+        function success() {
+            alert("수정 완료.");
+            location.replace("/articles/" + id);
+        }
+        function fail() {
+            alert("수정 실패.");
+            location.replace("/articles/" + id);
+        }
+        httpRequest("PUT", "/api/articles/" + id, body, success, fail);
     });
 }
 
@@ -47,20 +47,71 @@ if (modifyButton) {
 const createButton = document.getElementById('create-btn');
 
 if (createButton) {
-    createButton.addEventListener('click', event => {
-       fetch("/api/articles", {
-           method: "POST",
-           headers: {
-               "Content-Type": "application/json",
-           },
-           body: JSON.stringify({
-               title: document.getElementById('title').value,
-               content: document.getElementById('content').value,
-           }),
-       })
-       .then(() => {
-           alert('등록 완료');
-           location.replace("/articles");
-       });
+    createButton.addEventListener("click", (event) => {
+        let body = JSON.stringify({
+            title: document.getElementById("title").value,
+            content: document.getElementById("content").value,
+        });
+        function success() {
+            alert("등록 완료.");
+            location.replace("/articles");
+        }
+        function fail() {
+            alert("등록 실패.");
+            location.replace("/articles");
+        }
+        httpRequest("POST", "/api/articles", body, success, fail);
     });
+}
+
+function httpRequest(method, url, body, success, fail) {
+    fetch(url, {
+        method: method,
+        headers: {
+          Authorization: "Bearer "+ localStorage.getItem("access_token"),
+          "Content-Type": "application/json",
+        },
+        body: body,
+    }).then((response) => {
+        if (response.status === 200 || response.status === 201) {
+            return success();
+        }
+        const refresh_token = getCookie("refresh_token");
+        if (response.status === 401 && refresh_token) {
+            fetch("/api/token", {
+                method: "POST",
+                headers: {
+                    Authorization: "Bearer " + localStorage.getItem("access_token"),
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    refreshToken: getCookie("refresh_token"),
+                }),
+            }).then((res) => {
+                if (res.ok) {
+                    return res.json();
+                }
+            }).then(result => {
+                localStorage.setItem('access_token', result.accessToken);
+                httpRequest(method, url, body, success, fail);
+            }).catch((error) => fail());
+        } else {
+            return fail();
+        }
+    });
+}
+
+function getCookie(key) {
+    let result = null;
+    let cookie = document.cookie.split(";");
+    cookie.some(function (item) {
+        item = item.replace(" ", "");
+        let dic = item.split("=");
+
+        if (key === dic[0]) {
+            result = dic[1];
+            return true;
+        }
+    });
+    return result;
 }
